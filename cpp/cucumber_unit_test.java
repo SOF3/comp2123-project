e4001922c84724ce78053e82bb4745975c0a83b6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-23 12:14:01 +0000

    Deploy chankyin/comp2123-project to github.com/chankyin/comp2123-project.git:travis

---
+++ cpp/cucumber_unit_test.java
@@ -0,0 +1,17 @@
+@Given("^today is Sunday$")
+public void today_is_Sunday() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@When("^I ask whether it's Friday yet$")
+public void i_ask_whether_it_s_Friday_yet() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Then("^I should be told \"([^\"]*)\"$")
+public void i_should_be_told(String arg1) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
