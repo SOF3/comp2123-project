1f7a2fa59d8d5da930e6eb5e349683ea0413ece8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-23 15:18:40 +0000

    Deploy chankyin/comp2123-project to github.com/chankyin/comp2123-project.git:travis

---
+++ cpp/fibo1.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include "fibo1.h"
+
+int fibo(int times) {
+	int a = 1, b = 1;
+	if(times <= 0) throw std::string("input out of bounds"); // error value
+	if(times <= 2) return 1; // init condition
+	int i = 3;
+	while(true) {
+		int tmp = a + b;
+		a = b;
+		b = tmp;
+		if(i == times) break;
+		i++;
+	}
+	return b;
+}
