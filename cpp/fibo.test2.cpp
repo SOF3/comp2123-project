2143bf72f7658c0d0cb1f87ac84aae6b89627f53
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-23 15:29:26 +0000

    Deploy chankyin/comp2123-project to github.com/chankyin/comp2123-project.git:travis

--- cpp/fibo.test2.cpp
+++ cpp/fibo.test2.cpp
@@ -1,8 +1,19 @@
+#include <string>
 #include "assert.h"
 #include "assert_int.h"
 #include "fibo2.h"
 
 void testFibo() {
+	bool ok = false;
+	try {
+		fibo(-1);
+	} catch(std::string error) {
+		ok = true;
+	}
+	if(!ok) {
+		std::cerr << "fibo(-1) does not throw an exception!" << std::endl;
+	}
 	ASSERT_EQUAL(fibo(2), 1)
 	ASSERT_EQUAL(fibo(3), 2)
+	ASSERT_EQUAL(fibo(5), 5)
 }
