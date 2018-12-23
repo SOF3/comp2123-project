a069b5b6908604c07826d1c1328b59e64d41291a
m2 <m2@m2-surviv-io.m2>
2018-12-23 21:49:42 +0800

    calcualtor stuff

---
+++ cpp/Fake_BigInteger.h
@@ -0,0 +1,32 @@
+#ifndef BIGINTEGER_H
+#define BIGINTEGER_H
+#include <string>
+using namespace std;
+
+class BigInteger {
+	long long val;
+	public:
+		BigInteger(int a){
+			val = a;
+		}
+		BigInteger operator+(BigInteger rhs){
+			return val + rhs.val;
+		}
+		BigInteger operator-(BigInteger rhs){
+			return val - rhs.val;
+		}
+		BigInteger operator*(BigInteger rhs){
+			return val * rhs.val;
+		}
+		BigInteger operator/(BigInteger rhs){
+			return val / rhs.val;
+		}
+		BigInteger operator>(BigInteger rhs){
+			// implementation omitted
+		}
+		int value(){
+			return (int) val;
+		}
+};
+
+#endif
---
+++ cpp/calculator3.cpp
@@ -0,0 +1,17 @@
+#include "Calculator.h"
+#include "BigInteger.h"
+
+int eval(int a, int b, char operation){
+	BigInteger bigA(a), BigInteger bigB(b);
+	BigInteger result(0);
+
+	if (operation == '+') result = bigA + bigB;
+	if (operation == '-') result = bigA + bigB;
+	if (operation == '*') result = bigA + bigB;
+	if (operation == '/') result = bigA + bigB;
+
+	if (result > BigInteger((1<<31)-1))
+		throw std::overflow_error("too big");
+	else
+		return result.value();
+}
